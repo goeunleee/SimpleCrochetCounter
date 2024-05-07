31e272286efa6687f1ecce18acf4879a379fdec6
goeunlee <goeun3501@gmail.com>
2024-05-07 09:27:34 +0900

    [fix]임시커밋

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+// ignore_for_file: unused_import, unused_field, avoid_print, prefer_typing_uninitialized_variables
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:math';
+import 'package:roulette/roulette.dart';
+import 'package:arrow/arrow.dart';
 
 void main() {
   runApp(const App());
@@ -36,28 +41,92 @@ class App extends StatefulWidget {
   State<App> createState() => _AppState();
 }
 
-class _AppState extends State<App> {
+class _AppState extends State<App> with SingleTickerProviderStateMixin {
   late SharedPreferences prefs;
-  int cnt = 0;
 
+  int cnt = 0;
+  static final _random = Random();
+
+  late RouletteController _controller;
+  late TextEditingController _textController;
+  final bool _clockwise = true;
   Future initPrefs() async {
     prefs = await SharedPreferences.getInstance();
+
     final prev = prefs.getInt("cnt");
+    final memo = prefs.getString("memo1");
     setState(() {});
     if (prev != null) {
       counter = prev;
     } else {
       counter = 0;
     }
-    print(prefs.getInt("cnt"));
+    if (memo == null) {
+      text1 = "";
+    } else {
+      text1 = memo;
+    }
+    // print(prefs.getInt("cnt"));
+    print("memo$memo");
+    _textController = TextEditingController(text: text1);
   }
 
   int counter = 0;
+  String text1 = "";
+  final colors = <Color>[
+    Colors.red.withAlpha(50),
+    Colors.green.withAlpha(30),
+    Colors.blue.withAlpha(70),
+    Colors.yellow.withAlpha(90),
+    Colors.amber.withAlpha(50),
+    Colors.indigo.withAlpha(70),
+  ];
+
+  final icons = <IconData>[
+    Icons.ac_unit,
+    Icons.access_alarm,
+    Icons.access_time,
+    Icons.accessibility,
+    Icons.account_balance,
+    Icons.account_balance_wallet,
+  ];
+
+  final images = <ImageProvider>[
+    // Use [AssetImage] if you have 2.0x, 3.0x images,
+    // We only have 1 exact image here
+    const ExactAssetImage("asset/gradient.jpg"),
+    const NetworkImage("https://picsum.photos/seed/example1/400"),
+    const ExactAssetImage("asset/gradient.jpg"),
+    const NetworkImage("https://bad.link.to.image"),
+    const ExactAssetImage("asset/gradient.jpg"),
+    const NetworkImage("https://picsum.photos/seed/example5/400"),
+    // MemoryImage(...)
+    // FileImage(...)
+    // ResizeImage(...)
+  ];
   @override
   void initState() {
     super.initState();
     setState(() {});
     initPrefs();
+    assert(colors.length == icons.length);
+    assert(colors.length == images.length);
+
+    _controller = RouletteController(
+      vsync: this,
+      group: RouletteGroup.uniformImages(
+        colors.length,
+        colorBuilder: (index) => colors[index],
+        imageBuilder: (index) => images[index],
+        textBuilder: (index) {
+          if (index == 0) return 'i0';
+          return 'else';
+        },
+        styleBuilder: (index) {
+          return const TextStyle(color: Colors.black);
+        },
+      ),
+    );
   }
 
   void increase() async {
@@ -100,7 +169,7 @@ class _AppState extends State<App> {
           appBar: AppBar(
             centerTitle: true,
             title: const Text(
-              '몇 단 까지 떴더라?',
+              'simple crochet counter',
             ),
             backgroundColor: Colors.green.shade100,
           ),
@@ -148,8 +217,78 @@ class _AppState extends State<App> {
                     iconSize: 45,
                     icon: const Icon(Icons.replay_circle_filled_rounded),
                   ),
-                  const SizedBox(
-                    height: 70,
+                  Form(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 100),
+                      child: Column(
+                        children: [
+                          const SizedBox(
+                            height: 70,
+                          ),
+                          const Text(
+                            'list',
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: true,
+                                onChanged: (val) {},
+                              ),
+                              Flexible(
+                                child: TextFormField(
+                                  controller: _textController,
+                                  onChanged: (text) {
+                                    //  prefs.setInt("cnt", 0);
+                                    // prefs.setString("memo1", text);
+                                  },
+                                  maxLength: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: true,
+                                onChanged: (val) {},
+                              ),
+                              Flexible(
+                                child: TextFormField(
+                                  maxLength: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: true,
+                                onChanged: (val) {},
+                              ),
+                              Flexible(
+                                child: TextFormField(
+                                  maxLength: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            children: [
+                              Checkbox(
+                                value: true,
+                                onChanged: (val) {},
+                              ),
+                              Flexible(
+                                child: TextFormField(
+                                  maxLength: 20,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
                   const Padding(
                       padding: EdgeInsets.all(30),
@@ -186,6 +325,18 @@ class _AppState extends State<App> {
                       )),
                   Image.asset('assets/images/sign.png',
                       width: 800, height: 550),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                  Roulette(controller: _controller),
+                  FloatingActionButton(
+                    onPressed: () => _controller.rollTo(
+                      3,
+                      clockwise: _clockwise,
+                      offset: _random.nextDouble(),
+                    ),
+                    child: const Icon(Icons.refresh_rounded),
+                  ),
                 ],
               ),
             ),
