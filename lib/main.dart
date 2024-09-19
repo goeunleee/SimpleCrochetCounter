24c8dcd804fb01593e90ae8283079b5ed35bb484
goeun lee <goeun3501@gmail.com>
2024-09-19 10:08:44 +0900

    [feat]프로젝트 타이틀 기능 추가, 룰렛 편집기능 저장버튼 안보이는 오류 있음

--- lib/main.dart
+++ lib/main.dart
@@ -50,11 +50,45 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
   late RouletteController _controller;
   late TextEditingController _textController;
   final bool _clockwise = true;
+
+  bool isEditingTitle = false;
+  String title = "";
+
+  List<String> slotTexts = [
+    "Item 1",
+    "Item 2",
+    "Item 3",
+    "Item 4",
+    "Item 5",
+    "Item 6"
+  ];
+
+  List<TextEditingController> slotControllers = []; // 여기서 선언
+
+  List<bool> isEditingSlot = [false, false, false, false, false, false];
+
+  final List<Color> colors = [
+    Colors.red.withOpacity(0.5),
+    Colors.green.withOpacity(0.5),
+    Colors.blue.withOpacity(0.5),
+    Colors.yellow.withOpacity(0.5),
+    Colors.amber.withOpacity(0.5),
+    Colors.indigo.withOpacity(0.5),
+  ];
+
   Future initPrefs() async {
     prefs = await SharedPreferences.getInstance();
 
     final prev = prefs.getInt("cnt");
     final memo = prefs.getString("memo1");
+
+    final savedTitle = prefs.getString("title");
+    if (savedTitle == null) {
+      title = "untitled project";
+    } else {
+      title = savedTitle;
+    }
+
     setState(() {});
     if (prev != null) {
       counter = prev;
@@ -66,21 +100,22 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
     } else {
       text1 = memo;
     }
-    // print(prefs.getInt("cnt"));
+
     print("memo$memo");
     _textController = TextEditingController(text: text1);
   }
 
+  void saveTitle() async {
+    setState(() {
+      title = _textController.text.isEmpty ? "제목없음" : _textController.text;
+      isEditingTitle = false;
+    });
+    await prefs.setString("title", title);
+    print("Title saved: $title");
+  }
+
   int counter = 0;
   String text1 = "";
-  final colors = <Color>[
-    Colors.red.withAlpha(50),
-    Colors.green.withAlpha(30),
-    Colors.blue.withAlpha(70),
-    Colors.yellow.withAlpha(90),
-    Colors.amber.withAlpha(50),
-    Colors.indigo.withAlpha(70),
-  ];
 
   final icons = <IconData>[
     Icons.ac_unit,
@@ -91,40 +126,22 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
     Icons.account_balance_wallet,
   ];
 
-  final images = <ImageProvider>[
-    // Use [AssetImage] if you have 2.0x, 3.0x images,
-    // We only have 1 exact image here
-    const ExactAssetImage("asset/gradient.jpg"),
-    const NetworkImage("https://picsum.photos/seed/example1/400"),
-    const ExactAssetImage("asset/gradient.jpg"),
-    const NetworkImage("https://bad.link.to.image"),
-    const ExactAssetImage("asset/gradient.jpg"),
-    const NetworkImage("https://picsum.photos/seed/example5/400"),
-    // MemoryImage(...)
-    // FileImage(...)
-    // ResizeImage(...)
-  ];
   @override
   void initState() {
     super.initState();
     setState(() {});
     initPrefs();
     assert(colors.length == icons.length);
-    assert(colors.length == images.length);
 
+    for (int i = 0; i < slotTexts.length; i++) {
+      slotControllers.add(TextEditingController(text: slotTexts[i]));
+    }
     _controller = RouletteController(
       vsync: this,
-      group: RouletteGroup.uniformImages(
-        colors.length,
-        colorBuilder: (index) => colors[index],
-        imageBuilder: (index) => images[index],
-        textBuilder: (index) {
-          if (index == 0) return 'i0';
-          return 'else';
-        },
-        styleBuilder: (index) {
-          return const TextStyle(color: Colors.black);
-        },
+      group: RouletteGroup.uniform(
+        slotTexts.length, // 슬롯 개수
+        colorBuilder: (index) => colors[index], // 슬롯 색상
+        textBuilder: (index) => slotTexts[index], //  슬롯 텍스트
       ),
     );
   }
@@ -161,186 +178,259 @@ class _AppState extends State<App> with SingleTickerProviderStateMixin {
     prefs.setInt("cnt", 0);
   }
 
+  // 슬롯 텍스트 편집 UI
+  Widget buildSlotEditor(int index) {
+    return isEditingSlot[index]
+        ? Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: slotControllers[index], // 각 슬롯의 컨트롤러 사용
+                    autofocus: true,
+                    onChanged: (newText) {
+                      setState(() {
+                        slotTexts[index] = newText; // 텍스트 변경 반영
+                      });
+                    },
+                  ),
+                ),
+                // 저장 버튼 추가
+                IconButton(
+                  icon: const Icon(Icons.save), // 저장 아이콘
+                  onPressed: () {
+                    setState(() {
+                      isEditingSlot[index] = false; // 편집 모드 종료
+                      // 변경된 텍스트를 저장
+                    });
+                  },
+                ),
+              ],
+            ),
+          )
+        : GestureDetector(
+            onTap: () {
+              setState(() {
+                isEditingSlot[index] = true; // 슬롯 편집 모드 활성화
+              });
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              child: Text(
+                slotTexts[index],
+                style: const TextStyle(fontSize: 16),
+              ),
+            ),
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: const Color(0xfff4eddb),
-          appBar: AppBar(
-            centerTitle: true,
-            title: const Text(
-              'simple crochet counter',
-            ),
-            backgroundColor: Colors.green.shade100,
+        backgroundColor: const Color(0xfff4eddb),
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text(
+            'simple crochet counter',
           ),
-          body: SingleChildScrollView(
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(
-                    height: 15,
+          backgroundColor: Colors.green.shade100,
+        ),
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 40),
+
+                // 제목 수정
+                isEditingTitle
+                    ? Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              autofocus: true,
+                              onChanged: (newTitle) {
+                                title = newTitle;
+                              },
+                              controller: _textController,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: saveTitle, // 저장 버튼
+                            icon: const Icon(Icons.save),
+                          ),
+                        ],
+                      )
+                    : GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            isEditingTitle = true;
+                          });
+                        },
+                        child: Text(
+                          title.isEmpty ? "제목없음" : title,
+                          style: const TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+
+                const SizedBox(height: 15),
+
+                const Text(
+                  "you did",
+                  style: TextStyle(
+                    fontSize: 20,
                   ),
-                  const Text(
-                    "Click Count",
-                    style: TextStyle(
-                      fontSize: 20,
+                ),
+                Text(
+                  "$counter",
+                  style: const TextStyle(
+                    fontSize: 30,
+                  ),
+                ),
+
+                // 카운터 버튼들
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      onPressed: increase,
+                      icon: const Icon(Icons.cruelty_free_outlined),
+                      iconSize: 45,
                     ),
-                  ),
-                  Text(
-                    "$counter",
-                    style: const TextStyle(
-                      fontSize: 30,
+                    const SizedBox(
+                      width: 20,
                     ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                    IconButton(
+                      onPressed: decrease,
+                      icon: const Icon(Icons.cruelty_free_sharp),
+                      iconSize: 45,
+                    ),
+                  ],
+                ),
+
+                IconButton(
+                  onPressed: refresh,
+                  iconSize: 45,
+                  icon: const Icon(Icons.replay_circle_filled_rounded),
+                ),
+
+                const Padding(
+                  padding: EdgeInsets.all(30),
+                  child: Column(
                     children: [
-                      IconButton(
-                        onPressed: increase,
-                        icon: const Icon(Icons.cruelty_free_outlined),
-                        iconSize: 45,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Button(name: "slip/ss"),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Button(name: "ch"),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Button(name: "sc"),
+                        ],
                       ),
-                      const SizedBox(
-                        width: 20,
+                      SizedBox(
+                        height: 15,
                       ),
-                      IconButton(
-                        onPressed: decrease,
-                        icon: const Icon(Icons.cruelty_free_sharp),
-                        iconSize: 45,
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Button(name: "half-double"),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Button(name: "double"),
+                        ],
                       ),
                     ],
                   ),
-                  // for (var n in numbers) Text('$n'),
-                  IconButton(
-                    onPressed: refresh,
-                    iconSize: 45,
-                    icon: const Icon(Icons.replay_circle_filled_rounded),
-                  ),
-                  Form(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 100),
-                      child: Column(
-                        children: [
-                          const SizedBox(
-                            height: 70,
-                          ),
-                          const Text(
-                            'list',
-                            style: TextStyle(fontSize: 24),
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (val) {},
-                              ),
-                              Flexible(
-                                child: TextFormField(
-                                  controller: _textController,
-                                  onChanged: (text) {
-                                    //  prefs.setInt("cnt", 0);
-                                    // prefs.setString("memo1", text);
-                                  },
-                                  maxLength: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (val) {},
-                              ),
-                              Flexible(
-                                child: TextFormField(
-                                  maxLength: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (val) {},
-                              ),
-                              Flexible(
-                                child: TextFormField(
-                                  maxLength: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            children: [
-                              Checkbox(
-                                value: true,
-                                onChanged: (val) {},
-                              ),
-                              Flexible(
-                                child: TextFormField(
-                                  maxLength: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
+                ),
+
+                // 이미지 표시
+                Image.asset('assets/images/sign.png', width: 800, height: 550),
+
+                // 룰렛 표시 및 슬롯 텍스트 편집 가능
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.7,
+                      height: MediaQuery.of(context).size.width * 0.7,
+                      child: Roulette(
+                        controller: _controller,
                       ),
                     ),
-                  ),
-                  const Padding(
-                      padding: EdgeInsets.all(30),
-                      child: Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Button(name: "slip/ss"),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Button(name: "ch"),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Button(name: "sc"),
-                            ],
-                          ),
-                          SizedBox(
-                            height: 15,
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Button(name: "half-double"),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Button(name: "double"),
-                            ],
-                          ),
-                        ],
-                      )),
-                  Image.asset('assets/images/sign.png',
-                      width: 800, height: 550),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Roulette(controller: _controller),
-                  FloatingActionButton(
-                    onPressed: () => _controller.rollTo(
-                      3,
-                      clockwise: _clockwise,
-                      offset: _random.nextDouble(),
+                    Positioned(
+                      top: -45,
+                      child: Transform.scale(
+                        scaleY: 1.5,
+                        scaleX: 0.7,
+                        child: const Icon(
+                          Icons.arrow_drop_down,
+                          size: 110,
+                          color: Colors.red,
+                        ),
+                      ),
                     ),
-                    child: const Icon(Icons.refresh_rounded),
-                  ),
-                ],
-              ),
+                    // click 버튼을 중앙에 배치
+                    FloatingActionButton(
+                      onPressed: () => _controller.rollTo(
+                        3,
+                        clockwise: _clockwise,
+                        offset: _random.nextDouble(),
+                      ),
+                      shape: const CircleBorder(),
+                      child: const Text("click"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 20),
+
+                Column(
+                  children: List.generate(slotTexts.length, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isEditingSlot[index] = true; // 슬롯 편집 모드 활성화
+                        });
+                      },
+                      child: isEditingSlot[index]
+                          ? TextField(
+                              controller:
+                                  TextEditingController(text: slotTexts[index]),
+                              autofocus: true,
+                              onSubmitted: (newText) {
+                                setState(() {
+                                  slotTexts[index] = newText.isEmpty
+                                      ? "Item ${index + 1}"
+                                      : newText; // 빈 값일 때 기본 텍스트로 설정
+                                  isEditingSlot[index] = false; // 편집 모드 종료
+                                });
+                              },
+                            )
+                          : Text(
+                              slotTexts[index],
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                    );
+                  }),
+                ),
+
+                // 룰렛 돌리는 버튼
+              ],
             ),
-          )),
+          ),
+        ),
+      ),
     );
   }
 }
