1e90917e5e4ee447a39002544658195a81e7e3c6
goeun lee <goeun3501@gmail.com>
2024-09-20 15:20:20 +0900

    fix..

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -28,8 +28,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "db38da832072a7f3aca961eddb4e2511
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "af637c067f18aea7935ea9001b2b6b70",
-"/": "af637c067f18aea7935ea9001b2b6b70",
+"index.html": "143b49f17048bd08ebfc644402243611",
+"/": "143b49f17048bd08ebfc644402243611",
 "main.dart.js": "3c3693c4e6aabd2d2cafb3c8d5ebcbd5",
 "manifest.json": "a50c3dc8982557ce2186054af91293d9",
 "version.json": "956bc5b24a32f701bebfb4c3502d5c02"};
